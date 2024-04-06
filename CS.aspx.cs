b277659947be31f522b58d32caefef314ba30a81
Arpit Nuwal <45119425+arpitnuwal@users.noreply.github.com>
2024-04-06 11:14:40 +0530

    Add files via upload

---
+++ CS.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+public partial class CS : System.Web.UI.Page
+{
+    protected void Upload(object sender, EventArgs e)
+    {
+
+        string filePath = Server.MapPath("~/Uploads/1.jpeg");
+
+        using (Document pdfDoc = new Document(PageSize.A4, 0, 0, 0, 0))
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, memoryStream);
+                pdfDoc.Open();
+                iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(filePath);
+
+                // Scale the image to fill the entire page
+                img.ScaleAbsolute(pdfDoc.PageSize.Width, pdfDoc.PageSize.Height);
+
+                pdfDoc.Add(img);
+                pdfDoc.Close();
+                Response.ContentType = "application/pdf";
+                Response.AddHeader("content-disposition", "attachment;filename=ImageExport.pdf");
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.BinaryWrite(memoryStream.ToArray());
+                Response.End();
+            }
+        }
+
+
+
+    }
+}
